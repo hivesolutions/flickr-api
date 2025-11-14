7ec6b8d44c9d872c8d90be32fc48f7afeb2c777e
João Magalhães <joamag@gmail.com>
2025-11-14 09:12:35 +0000

    feat: add test suite configuration to setup.py

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setuptools.setup(
     url="http://flickr-api.hive.pt",
     zip_safe=False,
     packages=["flickr"],
+    test_suite="flickr.test",
     package_dir={"": os.path.normpath("src")},
     install_requires=["appier"],
     classifiers=[
